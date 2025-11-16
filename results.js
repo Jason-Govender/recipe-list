29f8aaf7c2ebd3587ef5b08f5a893e248b0d9af5
Jason Govender <jasongovend@gmail.com>
2025-11-16 12:35:13 +0200

    Removed Pagination and added scroll overflow to search results

--- results.js
+++ results.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   const store = sessionStorage.getItem('recipeData');
   const parse = JSON.parse(store);
-  const recipes = parse.hits.slice(0, 6);
+  const recipes = parse.hits;
   renderRecipes(recipes);
 });
 
@@ -22,7 +22,6 @@ function renderRecipes(recipes) {
     const recipeCard = document.createElement('article');
     recipeCard.className = 'recipe-card';
     recipeCard.innerHTML = `
-     <section class="recipe-body">
       <img src="${recipe.image}" alt=${recipe.label} class="recipe-image"></img>
       <h2 class="recipe-title">${recipe.label}</h2>
       <p class="recipe-source">By ${recipe.source}</p>
@@ -34,7 +33,6 @@ function renderRecipes(recipes) {
         <i class="fa-solid fa-plus"></i>
         <i class="fa-regular fa-star"></i>
       </footer>
-     </section>
     `;
     container.appendChild(recipeCard);
   });
